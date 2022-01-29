682d871b5c14c6303ad9585e9ebfe1456459322a
Aaron Millikin <loofy@loofylabs.com>
2022-01-28 20:17:40 -0500

    minor tweak during day30 error handling

--- day26_natoalph/main.py
+++ day26_natoalph/main.py
@@ -4,8 +4,14 @@ nato_alphabet = pandas.read_csv("./nato_phonetic_alphabet.csv")
 
 nato_alphabet_dict = {row.letter:row.code for (index, row) in nato_alphabet.iterrows()}
 
-response = input("Enter a word: ").upper()
+def output_nato():
+    response = input("Enter a word: ").upper()
+    try: 
+        nato_output = [nato_alphabet_dict[letter] for letter in response]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        output_nato()
+    else:
+        print(f"{nato_output}")
 
-nato_output = [nato_alphabet_dict[letter] for letter in response]
-
-print(f"{nato_output}")
+output_nato()
