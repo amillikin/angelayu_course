2b0763a240c18c3c935aa6b196e25b4c44869d1a
Aaron Millikin <loofy@loofylabs.com>
2022-02-25 18:15:06 -0500

    day 59 final

---
+++ day57-59_blog/blog2.0/main.py
@@ -0,0 +1,45 @@
+import requests
+from flask import Flask, render_template
+from datetime import datetime
+from post import Post
+
+app = Flask(__name__)
+
+random_posts = requests.get(
+    "https://api.npoint.io/c790b4d5cab58020d391"
+).json()
+post_list = []
+for post in random_posts:
+    blog_post = Post(post["id"],
+                     post["title"],
+                     post["subtitle"],
+                     post["body"],
+                     "loofy",
+                     datetime.now().strftime("%Y-%m-%d"))
+    post_list.append(blog_post)
+
+
+@app.route("/")
+def get_index():
+    return render_template("index.html",
+                           posts=post_list)
+
+
+@app.route("/about")
+def get_about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def get_contact():
+    return render_template("contact.html")
+
+
+@app.route("/blog/<int:id>")
+def get_blog(id):
+    return render_template("post.html",
+                           post=post_list[id-1])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
