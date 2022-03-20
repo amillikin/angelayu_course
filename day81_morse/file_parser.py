7fd754e257575a1f44732a374e843a7a79e09d84
Aaron Millikin <loofy@loofylabs.com>
2022-03-19 23:49:12 -0400

    add file parsing

--- day81_morse/file_parser.py
+++ day81_morse/file_parser.py
@@ -1,31 +1,44 @@
-from morse_utils import decoder, input_validator
+from morse_utils import decoder, input_validator, joiner, tokenizer, prompt
+import re
 
 class FileParser:
 
 
-    def __init__(self, in_file, out_file):
+    def __init__(self, in_file, mode):
         self.in_file = in_file
-        self.out_file = out_file
+        self.mode = mode
+        self.message_in = ""
+        self.message_out = ""
         self.convert_input()
 
-    # def convert_input(self):
-    #     mode = ""
-    #     while mode not in ("T","M","Q"):
-    #         mode = input("(M)orse-to-Text\n"
-    #                      "(T)ext-to-Morse\n"
-    #                      "(Q)uit\n> ").upper()
-    #     if mode == "Q":
-    #         self.active = False
-    #     else:
-    #         message = input(prompt(mode)).strip().upper()
-    #         while not re.match(input_validator(mode), message):
-    #             message = input(prompt(mode)).strip().upper()
-    #             print(f"{message}")
-    #         token_list = tokenizer(mode, message)
-    #         try: 
-    #             converted_list = [decoder(mode)[token] for token in token_list]
-    #             converted_message = joiner(mode).join(converted_list)
-    #             print(f"{converted_message}")
-    #         except KeyError:
-    #             print("Invalid character or combination provided.\n"
-    #                   "Message could not be converted.")
+    def convert_input(self):
+        if self.mode == "tm":
+            self.parse_text("T")
+        elif self.mode == "mt":
+            self.parse_text("M")
+        elif self.mode == "wt":
+            self.parse_wave()
+        elif self.mode == "ww":
+            self.parse_wave()
+        else:
+            print(f"Mode {self.mode} is invalid.")
+
+    def parse_text(self, parse_mode):
+        try:
+            with open(self.in_file, "r") as file:
+                self.message_in = file.read().rstrip("\n")
+                token_list = tokenizer(parse_mode, self.message_in)
+                print(f"{token_list}")
+                try: 
+                    converted_list = [decoder(parse_mode)[token] for token in token_list]
+                    self.message_out = joiner(parse_mode).join(converted_list)
+                    print(f"{self.message_out}")
+                except KeyError:
+                    print("Invalid character or combination provided.\n"
+                            "self.message_in could not be converted.")
+        except FileNotFoundError:
+            print(f"Unable to open file {self.in_file}")
+
+
+    def parse_wave(self):
+        pass
--- day81_morse/main.py
+++ day81_morse/main.py
@@ -15,47 +15,26 @@ group.add_argument(
     action="store_true"
 )
 group.add_argument(
-    "-t",
-    "--textfile",
+    "-f",
+    "--file",
+    nargs=2,
+    type=str,
     help=
     """
-    
+    File to convert. |
+    Mode argument determines operation. |
+    tm: text to morse |
+    mt: morse to text |
+    tw: text to wav |
+    wt: wav to text |
+    e.g. python main.py -f message.txt tw
     """,
-    action="store_true"
 )
-group.add_argument(
-    "-w",
-    "--wavfile",
-    help=
-    """
-    Accepts a wav file, outputs a plaintext file of the same name
-    or if specified as the second positional argument.
-    """,
-    action="store_true"
-)
-
-group.add_argument("in_file",
-                   nargs="?",
-                   type=argparse.FileType('r'),
-                   default=sys.stdin,
-                   help="input file")
-group.add_argument("out_file",
-                   nargs="?",
-                   type=argparse.FileType('w'),
-                   default=sys.stdout,
-                   help="output file")
-
 
 args = parser.parse_args()
-
-#if not args.out_file:
-#    out_file = 
-
 if args.interactive:
     UserMode()
-# elif args.textfile:
-#     TextParser(args.in_file, args.files[1])
-# elif args.wavfile:
-#     WavParser(args.in_file, args.files[1])
+elif args.file:
+    FileParser(args.file[0], args.file[1])
 else:
     print(parser.parse_args(["-h"]))
