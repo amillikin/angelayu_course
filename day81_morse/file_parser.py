46ba6cb4c21f7860a1180fbaf1646597e4b98a12
Aaron Millikin <loofy@loofylabs.com>
2022-03-21 23:43:54 -0400

    start wave parsing

--- day81_morse/file_parser.py
+++ day81_morse/file_parser.py
@@ -46,20 +46,74 @@ class FileParser:
 
     def parse_wave(self):
         # TODO: how to read and interpret wave.
-        # Read header info.
-        # Set a threshold
-        # Check frame when threshold passed and when it falls again.
         # Calculate the duration of those intervals
         # Determine roughly how many different intervals we have
         # Use those to appropriately categorize each interval
         # Turn that into morse symbols, then to text
-        pass
+        try:
+            with wave.open(self.in_file, "rb") as file:
+                frequency = file.getframerate()
+                number_of_frames = file.getnframes()
+                data = file.readframes(number_of_frames)
+                data_segments = []
+                frame_number = 0
+                last_frame = (-1,-1) # (data_value, frame_pos)
+                
+                for frame in struct.iter_unpack("<h", data):
+                    if data_segments == []:
+                        data_segments.append(("on", frame_number))
+                    elif (data_segments[len(data_segments)-1][0] == "on" and
+                         last_frame[0] == 0 and
+                         frame[0] == 0):
+                            data_segments.append(("off", last_frame[1]))
+                    elif (data_segments[len(data_segments)-1][0] == "off" and
+                         frame[0] != 0):
+                            data_segments.append(("on", frame_number+1))
+                    else:
+                        pass
+                    frame_number += 1
+                    last_frame = (frame[0], frame_number)
+
+                tokens = []
+                for i in range(len(data_segments)-1):
+                    if data_segments[i][0] == "on":
+                        tokens.append(
+                            ("on",data_segments[i+1][1]-data_segments[i][1])
+                        )
+                    elif data_segments[i][0] == "off":
+                        tokens.append(
+                            ("off",data_segments[i+1][1]-data_segments[i][1])
+                        )
+                    else:
+                        pass
+                with open("test.txt", "w") as file:
+                    for token in tokens:
+                        file.write(f"{token}\n")
+                
+        except FileNotFoundError:
+            print(f"Unable to open file {self.in_file}")
 
 
     def output_text(self):
         with open("./output.txt", "w") as file:
             file.write(self.message_out)
 
+
+#   UNIT_LEN = .27 #seconds
+#   DOT = 1 * UNIT_LEN # dit
+#   DASH = 3 * UNIT_LEN # dah
+#   I_PAUSE = 1 * UNIT_LEN # inner pause
+#   L_PAUSE = 2 * UNIT_LEN # letter pause: followed by I_PAUSE, so 2+1=3
+#   W_PAUSE = 0 * UNIT_LEN # word pause: 
+#                          # surrounded by L_PAUSE, followed by I_PAUSE
+#                          # so 3+1+3=7u, don't technically need a frame here 
+#   
+#   SIGNAL_CONVERSION = {
+#       ".": DOT,
+#       "-": DASH,
+#       " ": L_PAUSE,
+#       "|": W_PAUSE, 
+#   }
 
     def output_wave(self):
         with wave.open("./output.wav", "wb") as file:
@@ -68,6 +122,7 @@ class FileParser:
             file.setframerate(SAMPLE_RATE)
             all_frames = []
             for symbol in self.message_out:
+                print(f"'{symbol}'")
                 data_frames = self.create_frames(
                     duration=SIGNAL_CONVERSION[symbol],
                     volume=VOLUME
